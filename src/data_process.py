87f4252b568e634b518f6a647d3ed6a43b1a83cf
Rohit Ranjan <rohitrjn629@gmail.com>
2021-11-18 00:53:50 +0530

    add toss win to match win count

---
+++ src/data_process.py
@@ -0,0 +1,23 @@
+# %%
+"""
+This file has to do with all data preprocessing needed for data.
+Then import corresping dataframe to required file.
+"""
+import pandas as pd
+import numpy as np
+
+# %%
+points_table = pd.read_csv('../data/merged/points_table.csv')
+season_batting_card = pd.read_csv('../data/merged/season_batting_card.csv')
+season_bowling_card = pd.read_csv('../data/merged/season_bowling_card.csv')
+season_details = pd.read_csv('../data/merged/season_details.csv', low_memory = False)
+season_summary = pd.read_csv('../data/merged/season_summary.csv')
+
+# %%
+points_table_df = pd.DataFrame(points_table)
+season_batting_card_df = pd.DataFrame(season_batting_card)
+season_bowling_card_df = pd.DataFrame(season_bowling_card)
+season_details_df = pd.DataFrame(season_details)
+season_summary_df = pd.DataFrame(season_summary)
+
+
---
+++ src/metadata.py
@@ -0,0 +1,16 @@
+team_color = {
+    'RCB' :'red',
+    'CSK' : '#FFFF3C',
+    'DC' : '#00008B',
+    'GL' : 'purple',
+    'KKR' :'violet',
+    'KXIP' :'#DCDDDF',
+    'Kochi' : 'green',
+    'MI' :'blue',
+    'PWI' : 'grey',
+    'RPS' : 'black',
+    'RR' : 'pink',
+    'SRH' : 'orange'
+}
+
+
