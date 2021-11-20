d468f4a564d494144636c733405ebe413105ec32
Rohit Ranjan <rohitrjn629@gmail.com>
2021-11-20 14:00:04 +0530

    add more toss plots

--- src/metadata.py
+++ src/metadata.py
@@ -13,5 +13,25 @@ team_color = {
     'SRH' : '#fb653f'
 }
 
+alt = 1
+def stage_score(row, team_code):
+    global alt
+    alt += 1
+    if 'Eliminator' in row['stage'] or '3Rd Place Play-Off' in row['stage'] or 'Elimination Final' in row['stage']:
+        return 7*((-1)**alt)
+    if 'Match' == row['stage'].split(' ')[1]:
+        return 6*((-1)**alt)
+    if 'Qualifier 2' in row['stage'] or '1St Qualifying Final' in row['stage']:
+        return 9*((-1)**alt)
+    if 'Qualifier 1' in row['stage'] or '2Nd Qualifying Final' in row['stage']:
+        return 8*((-1)**alt)
+    if 'Semi-Final' in row['stage']:
+        return 7.5*((-1)**alt)
+    if 'Final' in row['stage']:
+        if row['winner'] != team_code:
+            return 9.5*((-1)**alt)
+        return 10*((-1)**alt)
+    return 4*((-1)**alt)
+
 
 
